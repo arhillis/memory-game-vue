f644a88174416496ae421a28adbc6228df077c4c
Ann Hillis <andriellerhillis@gmail.com>
2021-04-21 17:33:06 -0500

    Got the modal to toggle

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-window.onload = () =>{
 
-    const game = new Vue({
-        el: '.container',
-        data: {
-            moves: 0,
-            mins: 0,
-            secs: 0,
-            playing: false,
-            faces: [
-                "fa-diamond",
-                "fa-paper-plane-o",
-                "fa-anchor",
-                "fa-bolt",
-                "fa-cube",
-                "fa-leaf",
-                "fa-bicycle",
-                "fa-bomb",
-                "fa-diamond",
-                "fa-paper-plane-o",
-                "fa-anchor",
-                "fa-bolt",
-                "fa-cube",
-                "fa-leaf",
-                "fa-bicycle",
-                "fa-bomb"
-            ],
-            showModal: false
+    const Game = ({
+        data(){
+            return {
+                moves: 0,
+                mins: 0,
+                secs: 0,
+                playing: false,
+                faces: [
+                    "fa-diamond",
+                    "fa-paper-plane-o",
+                    "fa-anchor",
+                    "fa-bolt",
+                    "fa-cube",
+                    "fa-leaf",
+                    "fa-bicycle",
+                    "fa-bomb",
+                    "fa-diamond",
+                    "fa-paper-plane-o",
+                    "fa-anchor",
+                    "fa-bolt",
+                    "fa-cube",
+                    "fa-leaf",
+                    "fa-bicycle",
+                    "fa-bomb"
+                ],
+                modalShown: false
+            }
+        },
+        methods: {
+            showModal(){this.modalShown = true},
+            hideModal(){this.modalShown = false}
         }
+        
     })
-}
+
+    Vue.createApp(Game).mount('.container');
