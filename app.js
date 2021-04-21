a912fa887ed39eafb7a5625d97229ae43edb3d1f
Ann Hillis <andriellerhillis@gmail.com>
2021-04-21 15:31:22 -0500

    Modal varible added

--- app.js
+++ app.js
@@ -3,7 +3,29 @@ window.onload = () =>{
     const game = new Vue({
         el: '.container',
         data: {
-            header: 'VUE WORKS!!!!'
+            moves: 0,
+            mins: 0,
+            secs: 0,
+            playing: false,
+            faces: [
+                "fa-diamond",
+                "fa-paper-plane-o",
+                "fa-anchor",
+                "fa-bolt",
+                "fa-cube",
+                "fa-leaf",
+                "fa-bicycle",
+                "fa-bomb",
+                "fa-diamond",
+                "fa-paper-plane-o",
+                "fa-anchor",
+                "fa-bolt",
+                "fa-cube",
+                "fa-leaf",
+                "fa-bicycle",
+                "fa-bomb"
+            ],
+            showModal: false
         }
     })
 }
